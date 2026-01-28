98af0d0cee1d72b455cd48e6a4d6072785241006
Ayeza Bashir <ayezabashir46@gmail.com>
2026-01-28 15:39:28 +0500

    help section

--- src/pages/support/Help.jsx
+++ src/pages/support/Help.jsx
@@ -1,7 +1,25 @@
-const Help = () => {
-  return (
-    <div>Help</div>
-  )
-}
+import { useMemo, useState } from "react";
+import helpData from "../../data/helpData.json";
+import PageHeader from "../../components/PageHeader";
+import HelpSearch from "../../components/Help/HelpSearch";
+import HelpList from "../../components/Help/HelpList";
 
-export default Help
+const Help = () => {
+  const [search, setSearch] = useState("");
+
+  const filteredHelp = useMemo(() => {
+    return helpData.filter((item) =>
+      item.question.toLowerCase().includes(search.toLowerCase()),
+    );
+  }, [search]);
+
+  return (
+    <div className="space-y-6 text-gray-900 dark:text-gray-200">
+      <PageHeader />
+      <HelpSearch search={search} setSearch={setSearch} />
+      <HelpList items={filteredHelp} />
+    </div>
+  );
+};
+
+export default Help;
